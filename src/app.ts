f7772c9e08e278abf5350076696ef6cf84c46b14
rontysarkar <rontysarkar2023@gmail.com>
2026-07-07 19:34:58 +0600

    create all modules and router

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
 import express, { Application, Request, Response } from 'express'
+import { authRoutes } from './modules/auth/auth.routes';
+import { propertyRoutes } from './modules/property/property.routes';
+import { categoryRoutes } from './modules/category/category.routes';
+import { landlordRoutes } from './modules/landlord/landlord.routes';
+import { rentalRoutes } from './modules/rental/rental.routes';
+
 
 const app :Application = express();
 
@@ -6,5 +12,16 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
+app.use('/api/auth',authRoutes)
+app.use('/api/properties',propertyRoutes)
+app.use('/api/categories',categoryRoutes)
+
+app.use('/api/landlord',landlordRoutes)
+app.use('/api/rentals',rentalRoutes)
+
+app.use('/api/payments')
+app.use('/api/reviews')
+
+app.use('/api/admin')
 
 export default app;
